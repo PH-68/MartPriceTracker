851555cbec1bb5bc7e7451d19328af3b509c8432
Poyi Hong <Poyi.Hong@outlook.com>
2021-01-21 10:16:17 +0800

    File Rename

--- PriceCrawler.py
+++ PriceCrawler.py
@@ -11,5 +11,3 @@ for i in soup.select("body > section > div.index-show.clearfix.commodities-list 
     print(i.contents[0]+ "  " + baseUrl + i.parent.attrs["href"])
     r = requests.get(baseUrl + i.parent.attrs["href"])
     
-
-    
